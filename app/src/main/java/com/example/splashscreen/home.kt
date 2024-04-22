f9a5c9192b98b5b480bda710529305ba7a2c3b77
firoj-mahamud <mdfirojmahamud514@gmail.com>
2024-04-23 02:12:02 +0600

    We Make A Splash Screen

---
+++ app/src/main/java/com/example/splashscreen/home.kt
@@ -0,0 +1,22 @@
+package com.example.splashscreen
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_home)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(packageCodePath:this,home::class.java))
+        },3000)
+
+        }
+    }
